35ca65bf01a295dd1c61fa8fd32b450a047f421b
Nishanth Koganti <nishanthkoganti@Nishanths-MacBook-Pro.local>
2021-02-14 11:44:10 +0530

    Add easter egg in the site

--- js/services/quizService.js
+++ js/services/quizService.js
@@ -12,7 +12,7 @@ angular.module('pictureQuiz')
     * Randomize array element order in-place.
     * Using Durstenfeld shuffle algorithm.
     */
-    for (var i = 20; i > 0; i--) {
+    for (var i = questions.length - 1; i > 0; i--) {
       var j = Math.floor(Math.random() * (i + 1));
       var temp = questions[i];
       questions[i] = questions[j];
