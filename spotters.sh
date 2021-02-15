e605ee2486a94cf75c0473404f4eef6ddbf4a0fb
Nishanth Koganti <nishanthkoganti@Nishanths-MacBook-Pro.local>
2021-02-15 09:49:32 +0530

    spotters.

---
+++ spotters.sh
@@ -0,0 +1,97 @@
+python bing_scraper.py --search 'TRACHEAL PAPPILOMATOSIS CT CHEST' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'HEMANGIOBLASTOMA MRI BRAIN AND SPINE' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'KUMMELLS OSTEONECROSIS XRAY SPINE' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'ATRETIC PARIETAL ENCEPHALOCELE MRI BRAIN' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'GASTRIC DIVERTICULUM CT ABDOMEN' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'TODDLERS FRACTURE XRAY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'BALLON KHYPHOPLASTY VERSUS VERTEBROPLASTY XRAY SPINE' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'COLOBOMATOUS CYST OF EYE CT' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'EWINGS SARCOMA XRAY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'STEINSTRASSE XRAY KUB' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'LIPPES LOOP XRAY PELVIS' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'FAT PAD SIGN ELBOW LATERAL VIEW OF ELBOW' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'PATENT VITELLOINTESTINAL DUCT LUMBILICAL CATHETERISATION' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'ROBERTS SIGN INFANTOGRAM' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'GRASTROJEJUNAL INTUSSUCEPTION CT ABDOMEN' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'LIEOTIASIS OSSEA XRAY FACE/PNS' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'METAPHYSEAL CORNER FRACTURE/BUCKET HANDLE FRACTURE XRAY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'THROMBOSED BASILAR ARTERY HYPERDENSE ON NCCT' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'INGUINOSCROTAL HERNIA XRAY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'THYMOLIPOMA CT CHEST AND MRI' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'PDA CLOSURE DEVICE AMPLATZ+XRAY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'ATLANTO OCCIPITAL DISSOCIATION INJURIES ATLANTO OCCIPITAL DISLOCATION+ATLANTO OCCIPITAL SUBLUXATION=LATERAL XRAY CERVICAL SPINE' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'ENTEROPATHY ASSOCIATED ARTHIRITIS BARIUM' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'SURGICALLY CREATED AVF IN ARM FOR DIALYSIS' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'MAISONNEUVE FRACTURE XRAY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'AURORA SIGN USG ABDOMEN' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'PLATYSPONDYLY + AAD = MPS  DOWNS XRAY SPINE' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'EMPYEMA NECESSITANS CT CHEST' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'OLIVOPONTOCEREBELLAR DEGENERATION MULTIPLE SYSTEM ATROPHY+MRI BRAIN' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'SUPERFICIAL SIDEROSIS MRI BRAIN' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'ECTRODACTYLY LOBSTER CLAW HAND DEFORMITY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'HOT CROSS BUN SIGN AXIAL T2 MRI' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'XRAY ABDOMEN OF CHILD SHOWING ADRENAL CALCIFICATION XRAY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'SPONDYLOEPIPHYSEAL TARDA LATERAL XRAY SPINE' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'SUDECKS DYSTROPHY/ COMPLEX REGIONAL PAIN SYNDROME' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'CAUDAL REGRESSION SYNDROME XRAY+MRI' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'TUBULAR RIBS XRAY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'NORMAL DIPLOIC LAKES INTRADIPLOIC LYTIC AREAS' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'SUBMUCOSAL FIBROID HSG' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'PNEUMOSINUS DILATANS WITH CALCIFIED MENINGIOMA ON LAT SKULL XRAY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'CLEIDOCRANIAL DYSPLASIA XRAY SKULL/PELVIS/SPINE/CHEST' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'DUVERNEY FRACTURE XRAY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'SQUAMOUS CELL CA OF EXTERNAL EAR CT' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'HOLT ORAM SYNDROME XRAY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'CONGENITAL HYPOTHYROIDISM XRAY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'CAECAL LIPOMA CECT ABDOMEN' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'PETROCALCANEAL BURSITIS  RA+AS+REITERS' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'SINUS PERICRANI' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'ADENOMYOMATOSIS GB' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'LUCKENSCHADEL/LACUNAR SKULL' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'SIALOLITHIASIS SIALOGRAPHY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'PRUNE BELLY SYNDROME XRAY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'BILIARY ASCARIASIS USG/BARIUM' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'COPPER BEATEN SKULL' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'SUBLINGUAL DERMOID CYST CT+MRI' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'EPIPHRENIC DIVERTICUM BARIUM SWALLOW' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'OSTEOPATHIA STRIATA (VOORHOEVE DISEASE) XRAY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'COLOBOMA OF EYE USG' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'OVARIAN HYPERSTIMULATION SYNDROME USG' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'GDA PSEUDOANEURYSM VERSUS CYSTIC ARTERY PSEUDOANEURYSM (BR OF RHA) CT+CONVENTIONAL ANGIOGRAPHY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'POLYARTERITIS NODOSA CT+CONVENTIONAL ANGIOGRAPHY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'TRACHEOBRONCHOPATHIA OSTEOCHONDROPLASTICA CT' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'HAGLUND DEFORMITY XRAY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'SOFT TISSUE HEMANGIOMA MRI' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'TRACHEAL AMYLOIDOSIS' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'ATRIAL FIBROMA MRI' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'CHORDOMA MRI+CT' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'NEUROMYELITIS OPTICA MRI' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'GIST CT' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'ARCH OF RIOLAN CONVENTIONAL ANGIOGRAPHY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'MARGINAL ARTERY OF DRUMMOND CONVENTIONAL ANGIOGRAPHY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'TRIGONOCEPAHLY XRAY+CT SKULL' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'PLEURAL PLOMBAGE XRAY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'HUTCHINSON/ CHAUFFEUR FRACTURE XRAY WRIST WITH FOREARM' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'RADIATION FIBROSIS HRCT CHEST' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'MEDIAN ARCUATE LIGAMENT SYNDROME/DUNBAR SYNDROME CT ANGIOGRAPHY SAGGITAL REFORMAT' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'KLIPPEL FEIL SYNDROME XRAY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'SIGMOID VOLVULUS XRAY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'CAECAL VOLVULUS' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'OMENTAL INFARCT CT' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'EPIPLOIC APPENDAGITIS CT' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'CA COLON BARIUM ENEMA' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'DIVERTICULOSIS WITH DIVERTICULITIS AND PELVIC ABCESS CT ABDOMEN' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'BENIGN STRICTURE OF ESOPHAGUS BARIUM SWALLOW' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'DIFFUSE ESOPHAGEAL SPASM BARIUM SWALLOW' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'ZENKER DIVERTICULUM BARIUM SWALLOW' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'HILAR CHOLANGIOCA - KLATSKIN TUMOR MRCP' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'CHOLEODOCHAL CYST MRCP' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'PRIMARY SCLEROSING CHOLANGITIS MRCP' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'BILIARY HAMARTOMAS VMC+MRCP' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'CAROLI DSWITH INTRADUCTAL CALCULUS' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'ISCHEMIC COLITIS XRAY+BARIUM' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'PORCELAIN GB CT+XRAY' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'ABC/GCT OF SACRUM' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'JEJUNOJEJUNAL INTUSSUCEPTION CT' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'CORPUS CALLOSIUM LIPOMA NCCT HEAD' --limit 5 --download --chromedriver ../chromedriver 
+python bing_scraper.py --search 'GOUT XRAY' --limit 5 --download --chromedriver ../chromedriver 
